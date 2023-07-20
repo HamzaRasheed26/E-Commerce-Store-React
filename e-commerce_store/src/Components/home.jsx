97893933c4f10a9a44c9d9c269f713d2885a181a
HamzaRasheed26 <hamzarasheed19961@gmail.com>
2023-07-21 01:02:21 +0500

    products card

--- e-commerce_store/src/Components/home.jsx
+++ e-commerce_store/src/Components/home.jsx
@@ -1,33 +1,51 @@
-import React from 'react'
-import {
-  MDBCarousel,
-  MDBCarouselItem,
-} from 'mdb-react-ui-kit';
-import Navbar from './navbar'
-import Footer from './footer'
-import ProductCards from './productCards';
-
+import React from "react";
+import { MDBCarousel, MDBCarouselItem } from "mdb-react-ui-kit";
+import Navbar from "./navbar";
+import Footer from "./footer";
+import ProductCards from "./productCards";
 
 export default function Home() {
   return (
-    <div >
+    <div>
       <Navbar />
-      
-        <h2
-          style={{
-            color: '#100f3d',
-            fontWeight: 'bold',
-            fontFamily: 'bahnschrift',
-            textAlign: 'center',
-            marginTop: '20px',
-            fontSize: '35px',
-          }} >
-          Previous Products
-        </h2>
 
-        <ProductCards/>
-        
+      <MDBCarousel showControls>
+        <MDBCarouselItem
+          className="w-100 d-block"
+          itemId={1}
+          src="/images/image1.jpg"
+          alt="..."
+        />
+        <MDBCarouselItem
+          className="w-100 d-block"
+          itemId={2}
+          src="/images/image2.png"
+          alt="..."
+        />
+        <MDBCarouselItem
+          className="w-100 d-block"
+          itemId={3}
+          src="/images/image3.png"
+          alt="..."
+        />
+      </MDBCarousel>
+
+      <h2
+        style={{
+          color: "#100f3d",
+          fontWeight: "bold",
+          fontFamily: "bahnschrift",
+          textAlign: "center",
+          marginTop: "20px",
+          fontSize: "35px",
+        }}
+      >
+        Previous Products
+      </h2>
+
+      <ProductCards />
+
       <Footer />
     </div>
-  )
+  );
 }
--- e-commerce_store/src/Components/productCards.jsx
+++ e-commerce_store/src/Components/productCards.jsx
@@ -1,33 +1,82 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function ProductCards() {
+  // State to hold the product data
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Fetch product data from the FakeStore API
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching product data:", error);
+      });
+  }, []);
+
+  const cardLineStyle = {
+    display: "flex",
+  };
+
+  const cardStyle = {
+    border: "1px solid black",
+    padding: "20px",
+    margin: "20px",
+    fontFamily: "bahnschrift",
+    width: "300px",
+  };
+
+  const imgStyle = {
+    height: "200px",
+    width: "200px",
+  };
+
+  const headingStyle = {
+    color: "#100f3d",
+    fontWeight: "bold",
+    textAlign: "center",
+  };
+
+  const starStyle = {
+    textAlign: "center",
+    marginTop: "-10px",
+  };
+
+  const priceStyle = {
+    color: "#f57224",
+    textAlign: "center",
+    fontWeight: "bold",
+  };
+
+  const btnStyle = {
+    color: "white",
+    backgroundColor: "#f57224",
+    borderRadius: "5px",
+  };
+
   return (
     <div>
-      <div className="cardLine">
-        <div className="card"
-        style={{height: '200px',}}>
-            <img src="/images/pro1.png" alt="" />
-            <h2>Smart Watch</h2>
-            <div style={{textAlign: 'center', marginTop: '-10px'}}>⭐⭐⭐⭐⭐</div>
-            <h2>4,999 Rs</h2>
-            <p style={{fontFamily: 'bahnschrift', textAlign: 'justify'}}>
-              Our Smart Watch combines style, functionality, and cutting-edge
-              technology to enhance your daily life. With its sleek design and
-              advanced features, it's more than just a timepiece.
-            </p>
+      {products.map((product) => (
+        <div className="cardLine" style={cardLineStyle}>
+          <div key={product.id} className="cardBox" style={cardStyle}>
             <center>
-              <button
-                style={{
-                  fontFamily: 'bahnschrift',
-                  color: 'white',
-                  backgroundColor: '#f57224',
-                }}
-              >
+              <img style={imgStyle} src={product.image} alt={product.title} />
+            </center>
+            <h2 style={headingStyle}>{product.title}</h2>
+            <div style={starStyle}>⭐⭐⭐⭐⭐</div>
+            <h2 style={priceStyle}>{product.price} Rs</h2>
+            <p style={{ textAlign: "justify" }}>{product.description}</p>
+            <center>
+              <button className="btn btn-outline-light" style={btnStyle}>
                 Buy Now
               </button>
             </center>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
-  )
+  );
 }
