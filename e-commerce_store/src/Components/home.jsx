959a6f5478a7aecd44779a89bf2e406b97fee852
HamzaRasheed26 <hamzarasheed19961@gmail.com>
2023-07-20 22:38:43 +0500

    Footer

--- e-commerce_store/src/Components/home.jsx
+++ e-commerce_store/src/Components/home.jsx
@@ -1,11 +1,53 @@
 import React from 'react'
+import {
+  MDBCarousel,
+  MDBCarouselItem,
+} from 'mdb-react-ui-kit';
 import Navbar from './navbar'
+import Footer from './footer'
+
 
 export default function Home() {
   return (
-    <div>
+    <div >
       <Navbar />
-      <h1>World</h1>
+      
+        <MDBCarousel showControls>
+          <MDBCarouselItem
+            className='w-100 d-block'
+            itemId={1}
+            src='/images/image1.jpg'
+            alt='...'
+          />
+          <MDBCarouselItem
+            className='w-100 d-block'
+            itemId={2}
+            src='/images/image2.jpg'
+            alt='...'
+          />
+          <MDBCarouselItem
+            className='w-100 d-block'
+            itemId={3}
+            src='/images/image3.jpg'
+            alt='...'
+          />
+        </MDBCarousel>
+
+        <h2
+          style={{
+            color: '#100f3d',
+            fontWeight: 'bold',
+            fontFamily: 'bahnschrift',
+            textAlign: 'center',
+            marginTop: '20px',
+            fontSize: '35px',
+          }} >
+          Previous Products
+        </h2>
+
+        
+        
+      <Footer />
     </div>
   )
 }
--- e-commerce_store/src/Components/navbar.jsx
+++ e-commerce_store/src/Components/navbar.jsx
@@ -3,27 +3,25 @@ import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 
 export default function Navbar() {
   return (
-    <div>
-        <nav style={{backgroundColor: '#100f3d', color: 'white'}}>
-            <div className="navtitle">
-                <img src="./images/cart.png" alt="" />
-                <h2>E-Commerce Store</h2>
-            </div>
+    <nav style={{backgroundColor: '#100f3d', color: 'white'}}>
+        <div className="navtitle">
+            <img src="/images/cart.png" alt="" />
+            <h2>E-Commerce Store</h2>
+        </div>
 
-            <div className="navlist">
-                <ul>
-                    <li><Link>Home</Link></li>
-                    <li><Link>Products</Link></li>
-                    <li><Link>Cart</Link></li>
-                    <li><Link>Contact Us</Link></li>
-                </ul>
-            </div>
+        <div className="navlist">
+            <ul>
+                <li><Link>Home</Link></li>
+                <li><Link>Products</Link></li>
+                <li><Link>Cart</Link></li>
+                <li><Link>Contact Us</Link></li>
+            </ul>
+        </div>
 
-            <div className="navbtn">
-                <button>Register</button>
-                <button>Login</button>
-            </div>
-        </nav>
-    </div>
+        <div className="navbtn">
+            <button>Register</button>
+            <button>Login</button>
+        </div>
+    </nav>
   )
 }
